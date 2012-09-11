c0668e05b6821452fdb273fbb5487358f499dafd
Ricardo Pacheco <ricardopacheco1208@gmail.com>
2012-09-11 04:14:43 -0300

    selectedphoto model tests ok

--- spec/models/selected_photo_spec.rb
+++ spec/models/selected_photo_spec.rb
@@ -11,6 +11,7 @@ describe SelectedPhoto do
   it { should respond_to(:title) }
   it { should respond_to(:photo) }
   it { should respond_to(:photo=) }
+  it { should respond_to(:as_json) }
   
   describe "#photo= should assign the photo" do
     before { @selected_photo.title = 'My Photo' }
@@ -20,6 +21,14 @@ describe SelectedPhoto do
   describe "#photo should return the selected photo" do
     before { @selected_photo.title = 'My Photo' }
     it { should == @selected_photo }
+  end
+  
+  describe "#as_json should include the id, title, and image urls" do
+    photo = Photo.create!(:image => File.new(Rails.root + 'app/assets/images/1.jpg'))
+    s = SelectedPhoto.new(:title => 'One Selected Title', :photo => photo)
+    s.as_json['image_gallery_url'].should == photo.image.url(:gallery)
+    s.as_json['image_large_url'].should == photo.image.url(:large)
+    s.as_json['image_original_url'].should == photo.image.url(:original)
   end
   
 end
