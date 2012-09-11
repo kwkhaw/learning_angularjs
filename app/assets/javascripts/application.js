1cb5acca9eded9e18f92b7236b28a094043735af
Ricardo Pacheco <ricardopacheco1208@gmail.com>
2012-09-11 01:59:51 -0300

    updating application.js for loading angular components

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,8 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require jquery.cycle
+//= require services
+//= require controllers
+//= require filters
+//= require widgets
 //= require_tree .
