99eb265bf5b733f123513e57a84559aa3532b4d1
Ricardo Pacheco <ricardopacheco1208@gmail.com>
2012-09-11 03:29:50 -0300

    add migration paperclip attachment image to photos

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120911060507) do
+ActiveRecord::Schema.define(:version => 20120911062729) do
 
   create_table "photos", :force => true do |t|
     t.string   "title"
     t.integer  "gallery_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "photos", ["gallery_id"], :name => "index_photos_on_gallery_id"
