e3224e0ea44248739197315c9fa5435dd0574cdb
Ricardo Pacheco <ricardopacheco1208@gmail.com>
2012-09-11 00:38:39 -0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LearningAngularjs::Application.config.secret_token = 'c7a7bc34e734b0f52bf08ed8b0958975d8fb37126c0daee0d26c5421b151a3292b12daa23f3d80fdcd793f2ac08b8fb4f5ec981ab25d5de8805eb43a9da415e8'
