1c6a2a665c5d80e6b95cc2823fd2c89df1a7c398
Ricardo Pacheco <ricardopacheco1208@gmail.com>
2012-09-13 01:46:33 -0300

    Add data seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+photographer1 = Photographer.create!(:name => 'Ricardo Pacheco')
+photographer2 = Photographer.create!(:name => 'Jupira Fotografo')
+
+gallery = photographer1.galleries.create!(:title => 'Galeria 1')
+6.times do |i|
+  gallery.photos.create!(:image => File.new(Rails.root + "app/assets/images/2_#{i + 1}.jpg"), :title => "Galeria 1 - Foto: #{i + 1}")
+end
+gallery = photographer2.galleries.create!(:title => 'Galeria 2')
+7.times do |i|
+  gallery.photos.create!(:image => File.new(Rails.root + "photos/1_#{i + 1}.jpg"), :title => "Galeria 2 - Foto: #{i + 1}")
+end
+gallery = photographer2.galleries.create!(:title => 'Galeria 3')
+8.times do |i|
+  gallery.photos.create!(:image => File.new(Rails.root + "photos/3_#{i + 1}.jpg"), :title => "Galeria 3 - Foto: #{i + 1}")
+end
